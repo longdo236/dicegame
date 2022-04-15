52bbdefd9fde097cf8e122abce3ba95fad87aad4
LongDo <longgooner236@gmail.com>
2022-04-15 16:52:08 +0700

    dicegame

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <div id="app">
+    <div class="wrapper clearfix">
+      <Players
+        v-bind:activePlayer="activePlayer"
+        v-bind:scoresPlayer="scoresPlayer"
+        v-bind:currentScore="currentScore"
+      />
+
+      <Controls v-on:handleNewGame="handleNewGame" />
+
+      <Dices />
+
+      <Popup-rule 
+        v-on:handleConfirm="handleConfirm"
+        v-bind:isOpenPopup="isOpenPopup"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Players from "./components/Players.vue";
+import Controls from "./components/Controls.vue";
+import Dices from "./components/Dices.vue";
+import PopupRule from "./components/PopupRule.vue";
+export default {
+  name: "app",
+  data() {
+    return {
+      isPlaying: false,
+      isOpenPopup: false,
+      activePlayer: 1, // Ai là người chơi hiện tại
+      scoresPlayer: [20, 50],
+      currentScore: 23,
+    };
+  },
+  components: {
+    Players,
+    Controls,
+    Dices,
+    PopupRule,
+  },
+  methods: {
+    handleConfirm() {
+      console.log("handleConfirm App.vue");
+    },
+    handleNewGame() {
+      console.log("handleNewGame App.vue");
+      // hiện thị Popup -> show luật chơi
+      this.isOpenPopup = true;
+    },
+  },
+};
+</script>
+
+<style>
+/**********************************************
+*** GENERAL
+**********************************************/
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.clearfix::after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+body {
+  background-image: linear-gradient(
+      rgba(62, 20, 20, 0.4),
+      rgba(62, 20, 20, 0.4)
+    ),
+    url("./public/assets/back.jpg");
+  background-size: cover;
+  background-position: center;
+  font-family: Lato;
+  font-weight: 300;
+  position: relative;
+  height: 100vh;
+  color: #555;
+}
+
+.wrapper {
+  width: 1000px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  box-shadow: 0px 10px 50px rgba(0, 0, 0, 0.3);
+  overflow: hidden;
+}
+</style>
---
+++ src/components/PopupRule.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="wrapper-popup" v-bind:class="getClassPopup">
+    
+    <div class="rule">
+      <h3>Luật chơi</h3>
+      <p>adsjkfjjlljljaljljfls</p>
+      <button 
+      v-on:click="confirm"
+      class="confirm">Đã hiểu</button>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Popup-rule",
+  props: {
+      isOpenPopup: {Boolean, default: false},
+  },
+  data() {
+    return {};
+  },
+  methods: {
+      confirm() {
+          console.log('confirm PopupRule.vue');
+          this.$emit('handleConfirm')
+          // truyền event ra App.vue
+      }
+  },
+  computed: {
+      getClassPopup: function(){
+          return {
+              'open-popup': this.isOpenPopup,
+          }
+      }
+  }
+};
+</script>
+
+<style>
+.wrapper-popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.4);
+  opacity: 0;
+  visibility: hidden;
+}
+.wrapper-popup.open-popup {
+  opacity: 1;
+  visibility: visible;
+  transition: all 0.3s ease;
+}
+.rule {
+  position: absolute;
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: white;
+  width: 350px;
+  padding: 20px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) scale(1.2);
+  transition: all 0.3s ease;
+}
+.open-popup .rule {
+  transform: translate(-50%, -50%) scale(1);
+}
+.rule h3 {
+  margin-bottom: 10px;
+}
+.rule .confirm {
+  cursor: pointer;
+  margin-top: 20px;
+  border: 2px solid black;
+  background-color: white;
+  padding: 8px 15px;
+  transition: all 0.3s ease;
+}
+.rule .confirm:hover {
+  color: white;
+  background-color: black;
+}
+/* kết hợp CSS với VueJS để xây dựng hiệu ứng - Zoom in - Zoom Out */
+</style>
